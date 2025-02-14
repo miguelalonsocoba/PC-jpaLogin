c99824e852fb793cd1dd4142506c0381e0e9b337
mac <miguetheking53@gmail.com>
2025-02-13 20:58:05 -0600

    Inicializando proyecto

---
+++ src/main/java/com/pc/login/jpaLogin/JpaLoginApplication.java
@@ -0,0 +1,13 @@
+package com.pc.login.jpaLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLoginApplication.class, args);
+	}
+
+}
